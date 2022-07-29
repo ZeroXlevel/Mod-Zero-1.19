25d44b5d44adc05a763becc116ca68395164fb4f
Cameron Groves <zerorobotastic@gmail.com>
2022-07-29 19:54:33 -0400

    Removed Message

--- src/main/java/net/zeroxlevel/modzero/ModZero.java
+++ src/main/java/net/zeroxlevel/modzero/ModZero.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//Can I See This?
 public class ModZero implements ModInitializer {
 	public static final String MOD_ID = "modzero";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
