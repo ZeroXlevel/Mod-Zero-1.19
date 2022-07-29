b4ab02fa02afa953b366ed22060f9509861ab851
Cameron Groves <zerorobotastic@gmail.com>
2022-07-29 19:52:55 -0400

    E

---
+++ src/main/java/net/zeroxlevel/modzero/ModZero.java
@@ -0,0 +1,16 @@
+package net.zeroxlevel.modzero;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//Can I See This?
+public class ModZero implements ModInitializer {
+	public static final String MOD_ID = "modzero";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/zeroxlevel/modzero/ModZeroClient.java
@@ -0,0 +1,10 @@
+package net.zeroxlevel.modzero;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModZeroClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
